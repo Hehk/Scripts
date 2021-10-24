f7fdb97021dd735ce54c8d268e4b3583a7e62934
Kyle Henderson <ona@lunch.run>
2021-10-24 14:43:56 -0500

    I got queries running on roam

---
+++ lib/roam.ts
@@ -0,0 +1,42 @@
+import puppeteer from 'puppeteer-core'
+
+type Brand<K, T> = K & { __brand: T }
+type RoamPage = Brand<puppeteer.Page, "RoamPage">
+
+export function run () {
+  console.log("FAILED")
+}
+
+type SetupRoam = {
+  password: string
+  email: string
+  graph: string
+}
+
+const delay = (ms : number) => new Promise(res => setTimeout(res, ms))
+
+export async function setupRoam({ password, email, graph } : SetupRoam) : RoamPage {
+  const roamApp = "https://roamresearch.com/#/app"
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: '/opt/homebrew/bin/chromium'
+  });
+  const page = await browser.newPage();
+  await page.goto(roamApp);
+  await page.waitForSelector(`input[name="email"]`)
+
+  await page.type(`input[name="email"]`, email)
+  await page.type(`input[name="password"]`, password)
+  await page.click(".bp3-button")
+
+  await page.goto(roamApp + `/${graph}`);
+
+  // It can take a while for roam to load
+  // TODO figure out a better way to make this work X.x
+  await delay(20_000)
+  return page as RoamPage
+}
+
+export async function query(roamPage : RoamPage, query : string) {
+  return await roamPage.evaluate(query => window.roamAlphaAPI.q(query), query)
+}
--- scripts/roam_to_anki/index.ts
+++ scripts/roam_to_anki/index.ts
@@ -1,1 +1,8 @@
-console.log("YO")
+import { setupRoam, query } from '../../lib/roam.ts'
+const password = process.env.PASSWORD
+const email = process.env.EMAIL
+
+setupRoam({ password, email, graph: "Hehk" }).then(roam => {
+  return query(roam, '[:find ?p ?title :where [?p :node/title ?title]]')
+}).then(console.log)
+
