66f90da34316a9cb795553757919e7abfef65fbf
Kyle Henderson <ona@lunch.run>
2021-10-24 12:05:27 -0500

    initial setup

---
+++ build.ts
@@ -0,0 +1,17 @@
+import * as esbuild from 'esbuild'
+import * as path from 'path'
+import { readdir } from 'fs/promises'
+
+const scriptsFolder = path.join(__dirname, 'scripts')
+
+readdir(scriptsFolder)
+.then(scripts => {
+  const build = scripts
+    .map(script => esbuild.build({
+      entryPoints: [path.join(scriptsFolder, script, 'index.ts')],
+      bundle: true,
+      platform: 'node',
+      outfile: path.join(__dirname, 'build',  script + '.js')
+    }))
+  return Promise.all(build)
+})
